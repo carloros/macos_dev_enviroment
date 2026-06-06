35148eeedf9729560404b6845a5d8357b721499f
carloros <carlosj.fab@gmail.com>
2026-06-06 13:07:49 +0200

    Added some backup for config files
    The plan is the follwoing:
    - Start with a fresh install
    - Install everything
    - Configure everything as I like
    - Back it up with Git and GH
    - Create a script to automate all or most of the process

---
+++ Sketchybar/sketchybar/plugins/space_windows.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+if [ "$SENDER" = "aerospace_workspace_change" ]; then
+  # Get all visible workspaces
+  visible_workspaces=$(aerospace list-workspaces --format "%{id} %{workspace-is-visible}" | grep "true" | awk '{print $1}')
+  
+  # Update only visible workspaces
+  for workspace in $visible_workspaces; do
+    is_focused=$(aerospace list-workspaces --format "%{id} %{workspace-is-focused}" | grep "^$workspace " | awk '{print $2}')
+    apps=$(aerospace list-windows --workspace "$workspace" | awk -F'|' '{gsub(/^ *| *$/, "", $2); print $2}')
+    
+    sketchybar --set space.$workspace drawing=on
+    
+    if [ "${apps}" != "" ]; then
+      icon_strip=" "
+      while read -r app; do
+        icon_strip+=" $($CONFIG_DIR/plugins/icon_map_fn.sh "$app")"
+      done <<<"${apps}"
+      sketchybar --set space.$workspace label="$icon_strip"
+    else
+      sketchybar --set space.$workspace label=""
+    fi
+  done
+fi
+
+monitor_index=1
+for monitor in $(aerospace list-monitors --format "%{monitor-appkit-nsscreen-screens-id}"); do
+  for sid in $(aerospace list-workspaces --monitor "$monitor"); do
+    sketchybar --add item space.$sid left \
+      --set space.$sid display="$monitor_index" \
+      --subscribe space.$sid aerospace_workspace_change \
+      --set space.$sid \
+      drawing=on \
+      background.color=0x44ffffff \
+      background.corner_radius=5 \
+      background.drawing=on \
+      background.border_color=0xAAFFFFFF \
+      background.border_width=0 \
+      background.height=25 \
+      icon="$sid" \
+      icon.padding_left=10 \
+      icon.shadow.distance=4 \
+      icon.shadow.color=0xA0000000 \
+      label.font="sketchybar-app-font:Regular:16.0" \
+      label.padding_right=20 \
+      label.padding_left=0 \
+      label.y_offset=-1 \
+      label.shadow.drawing=off \
+      label.shadow.color=0xA0000000 \
+      label.shadow.distance=4 \
+      click_script="aerospace workspace $sid" \
+      script="$CONFIG_DIR/plugins/aerospace.sh $sid"
+  done
+  monitor_index=$((monitor_index + 1))
+done
---
+++ ZSH/.zshrc
@@ -0,0 +1,53 @@
+# Clear
+alias cl='clear'
+
+# FZF
+source <(fzf --zsh)
+alias fzf='fzf --preview "bat --color=always --style=numbers --line-range=:500 {}"'
+
+# Cat
+alias cat=bat
+
+# List
+alias la=tree
+
+# Lazygit
+alias lg='lazygit'
+
+# Git
+alias gc="git commit -m"
+alias gca="git commit -a -m"
+alias gp="git push origin HEAD"
+alias gpu="git pull origin"
+alias gst="git status"
+alias glog="git log --graph --topo-order --pretty='%w(100,0,6)%C(yellow)%h%C(bold)%C(black)%d %C(cyan)%ar %C(green)%an%n%C(bold)%C(white)%s %N' --abbrev-commit"
+alias gdiff="git diff"
+alias gco="git checkout"
+alias gb='git branch'
+alias gba='git branch -a'
+alias gadd='git add'
+alias ga='git add -p'
+alias gcoall='git checkout -- .'
+alias gr='git remote'
+alias gre='git reset'
+alias nah="git reset --hard;git clean -df;"
+
+# Docker
+alias dco="docker compose"
+alias dps="docker ps"
+alias dpa="docker ps -a"
+alias dl="docker ps -l -q"
+alias dx="docker exec -it"
+
+# Dirs
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias ......="cd ../../../../.."
+
+# VIM
+alias v="/opt/homebrew/bin/nvim"
+
+# Nmap
+alias nm="nmap -sC -sV -oN nmap"
